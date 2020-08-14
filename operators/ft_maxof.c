9275e635f16f26ec3af7fad84c1902567ec1a936
Swenne Offerhaus <swennehacks@gmail.com>
2020-08-14 23:14:38 +0200

    handy functions to measure e return 1/0

---
+++ operators/ft_maxof.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_maxof.c                                         :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/06/19 22:03:30 by swofferh      #+#    #+#                 */
+/*   Updated: 2020/08/11 19:20:41 by sofferha      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+/*
+** FT_MAXOF (used on printf a lot) to check which int/len is bigger/higher
+*/
+
+int	ft_maxof(int v1, int v2)
+{
+	return ((v1 < v2) ? (v2) : (v1));
+}
