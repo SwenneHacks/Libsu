eb60fd957d43427f3e164b81ffaaf2752b04bfdb
Swenne Offerhaus <swennehacks@gmail.com>
2021-03-23 23:19:50 +0100

    comm.

--- printing/ft_putchar_fd.c
+++ printing/ft_putchar_fd.c
@@ -6,7 +6,7 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/08 13:05:21 by swofferh      #+#    #+#                 */
-/*   Updated: 2020/08/17 22:31:59 by sofferha      ########   odam.nl         */
+/*   Updated: 2021/03/22 23:09:24 by sofferha      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 /*
 ** FT_PUTCHAR_FD: Outputs the character c to given file descriptor.
 */
-
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
