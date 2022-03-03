d1076706ef4d259ec0971201661321d2ab84e6ae
Swenne @Codam <swofferh@student.codam.nl>
2022-03-03 15:37:55 +0100

    a better upgrade 2022

--- all/ft_strjoin.c
+++ all/ft_strjoin.c
@@ -6,40 +6,44 @@
 /*   By: swofferh <swofferh@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/11/13 20:47:54 by swofferh      #+#    #+#                 */
-/*   Updated: 2020/11/19 22:22:23 by sofferha      ########   odam.nl         */
+/*   Updated: 2022/02/16 16:37:07 by swofferh      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libsu.h"
 
 /*
-** FT_STRJOIN: Allocates malloc and returns a new
-** string, result of the concatenation of s1 and s2.
+** FT_STRJOIN: Joins one string to the end of another.
+** returns new string, the result after appending.
 */
 char			*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*result;
+	char	*new;
+	int		len1;
+	int		len2;
 	size_t	a;
 	size_t	b;
 
 	a = 0;
 	b = 0;
-	if (s1 == NULL || s2 == NULL)
+	if (!s1 || !s2)
 		return (NULL);
-	result = (char *)malloc((ft_strlen(s1) + ft_strlen(s2)) + 1);
-	if (result == NULL)
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	new = (char *)malloc((len1 + len2) + 1);
+	if (new == NULL)
 		return (NULL);
-	while (s1[a] != '\0')
+	while (s1[a])
 	{
-		result[a] = s1[a];
+		new[a] = s1[a];
 		a++;
 	}
-	while (s2[b] != '\0')
+	while (s2[b])
 	{
-		result[a] = s2[b];
+		new[a] = s2[b];
 		a++;
 		b++;
 	}
-	result[a] = '\0';
-	return (result);
+	new[a] = '\0';
+	return (new);
 }
